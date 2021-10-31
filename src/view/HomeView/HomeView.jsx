742d63778a2478934737ec4fa53cabe061843c93
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2021-10-31 10:05:45 +0200

    app

---
+++ src/view/HomeView/HomeView.jsx
@@ -0,0 +1,12 @@
+// import generalImage from "../../images/phone-book.png";
+import s from "./HomeView.module.css";
+
+export default function HomeView() {
+  return (
+    <>
+      <span className={s.message}>
+        Welcome! To use the Phone Book, please log in or sign up.
+      </span>
+    </>
+  );
+}
